74528168a40ee165b86f5639c8160c44da52f4e6
victorhung <victorhung777@gmail.com>
2020-12-26 23:16:57 +0800

    Practice how to write a class

--- src/main/java/com/victor/Hello.java
+++ src/main/java/com/victor/Hello.java
@@ -8,6 +8,21 @@ public class Hello {
         Person p = new Person();
         p.hello();
 
+        // Basic data type
+        int age = 19;
+        short age_16 = 19;
+        long age_64 = 19;
+        char char_16 = 'A';
+        byte byte_8 = 123;
+        float weight_32 = 66.5f;
+        double weight_64 = 66.5;
+        boolean enrooll = false;
 
+        // Wrapper Class: Build for basic type
+
+        // Reference data type
+        String name = "Tom";
+        Integer age_2 = 19;
+        Character char_obj = 'A';
     }
 }
--- src/main/kotlin/com/victor/Hello.kt
+++ src/main/kotlin/com/victor/Hello.kt
@@ -8,7 +8,19 @@ fun main() {
     Human().hello()
     val h = Human()  // What does val stand for?
     h.hello()
+
+    // There's no basic data type
+    // var or val
+    val age = 19
+    val age_2: Int = 20
+
+    var weight = 66.5
+    var weight_2 = 66.5
+    var name : String
+    name = "Hank"
+
 }
+// control + V to pop up the VCS window
 
 class Human {
     fun hello() {
